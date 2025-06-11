0c72f4293bd08faa6c2bd497517dbed8467fa2b4
Joy Malinda <kokimalinda@gmail.com>
2025-06-11 21:17:49 +0300

    Completed assignment

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,27 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 0
+    while i < 11:
+        print(f'{i}')
+        i += 1
+        print("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    new_nums = list()
+    for i in int_list:
+        new_num = i ** 2
+        new_nums.append(new_num)
+    return new_nums
+        
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(1,101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print ("Buzz")
+        else:
+            print(i)
